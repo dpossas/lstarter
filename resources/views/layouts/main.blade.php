14101db6e27275ce4a8283575075572feadcb23c
Douglas Possas <douglaspossas@gmai.com>
2017-11-27 15:25:02 -0200

    Correção do titulo do frontend

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>Stellar by HTML5 UP</title>
+		<title>lStarter - Laravel + InfyOm + ACL Nativa + Multi Idiomas</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
