d34995cee2c59fbcfe18e8c9d6b0b1e63c82060e
Douglas Possas <douglas.possas@artia.com>
2018-04-03 09:58:03 -0300

    Atuaização do site

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116407373-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-116407373-1');
+    </script>
+
     <meta charset="UTF-8">
     <title>Laravel Starter</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
@@ -16,146 +26,146 @@
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
     @yield('css')
-</head>
+  </head>
 
-<body class="skin-blue sidebar-mini">
-@if (!Auth::guest())
-    <div class="wrapper">
-        <!-- Main Header -->
-        <header class="main-header">
+  <body class="skin-blue sidebar-mini">
+  @if (!Auth::guest())
+      <div class="wrapper">
+          <!-- Main Header -->
+          <header class="main-header">
 
-            <!-- Logo -->
-            <a href="{{ route('admin.home') }}" class="logo">
-                <b>lStarter</b>
-            </a>
+              <!-- Logo -->
+              <a href="{{ route('admin.home') }}" class="logo">
+                  <b>lStarter</b>
+              </a>
 
-            <!-- Header Navbar -->
-            <nav class="navbar navbar-static-top" role="navigation">
-                <!-- Sidebar toggle button-->
-                <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-                    <span class="sr-only">Toggle navigation</span>
-                </a>
-                <!-- Navbar Right Menu -->
-                <div class="navbar-custom-menu">
-                    <ul class="nav navbar-nav">
-                        <li>
-                            @if ( App::isLocale('pt-BR') )
-                            <a href="{{ route('admin.locale.set',['locale' => 'en']) }}">English Version</a>
-                            @else
-                            <a href="{{ route('admin.locale.set',['locale' => 'pt-BR']) }}">Versão em Português</a>
-                            @endif
-                        </li>
-                        <!-- User Account Menu -->
-                        <li class="dropdown user user-menu">
-                            <!-- Menu Toggle Button -->
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <!-- The user image in the navbar-->
-                                <img src="{{ asset('images/logo.png') }}"
-                                     class="user-image" alt="User Image"/>
-                                <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                                <span class="hidden-xs">{!! Auth::user()->name !!}</span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <!-- The user image in the menu -->
-                                <li class="user-header">
-                                    <img src="{{ asset('images/logo.png') }}"
-                                         class="" alt="User Image"/>
-                                    <p>
-                                        {!! Auth::user()->name !!}
-                                        <small>@lang('app.labels.member_since') {!! Auth::user()->created_at->format('M. Y') !!}</small>
-                                    </p>
-                                </li>
-                                <!-- Menu Footer-->
-                                <li class="user-footer">
-                                    <div class="pull-left">
-                                        <a href="#" class="btn btn-default btn-flat">@lang('app.buttons.profile')</a>
-                                    </div>
-                                    <div class="pull-right">
-                                        <a href="{!! url('/logout') !!}" class="btn btn-default btn-flat"
-                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                            @lang('app.buttons.sign_out')
-                                        </a>
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </div>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+              <!-- Header Navbar -->
+              <nav class="navbar navbar-static-top" role="navigation">
+                  <!-- Sidebar toggle button-->
+                  <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                      <span class="sr-only">Toggle navigation</span>
+                  </a>
+                  <!-- Navbar Right Menu -->
+                  <div class="navbar-custom-menu">
+                      <ul class="nav navbar-nav">
+                          <li>
+                              @if ( App::isLocale('pt-BR') )
+                              <a href="{{ route('admin.locale.set',['locale' => 'en']) }}">English Version</a>
+                              @else
+                              <a href="{{ route('admin.locale.set',['locale' => 'pt-BR']) }}">Versão em Português</a>
+                              @endif
+                          </li>
+                          <!-- User Account Menu -->
+                          <li class="dropdown user user-menu">
+                              <!-- Menu Toggle Button -->
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                  <!-- The user image in the navbar-->
+                                  <img src="{{ asset('images/logo.png') }}"
+                                       class="user-image" alt="User Image"/>
+                                  <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                                  <span class="hidden-xs">{!! Auth::user()->name !!}</span>
+                              </a>
+                              <ul class="dropdown-menu">
+                                  <!-- The user image in the menu -->
+                                  <li class="user-header">
+                                      <img src="{{ asset('images/logo.png') }}"
+                                           class="" alt="User Image"/>
+                                      <p>
+                                          {!! Auth::user()->name !!}
+                                          <small>@lang('app.labels.member_since') {!! Auth::user()->created_at->format('M. Y') !!}</small>
+                                      </p>
+                                  </li>
+                                  <!-- Menu Footer-->
+                                  <li class="user-footer">
+                                      <div class="pull-left">
+                                          <a href="#" class="btn btn-default btn-flat">@lang('app.buttons.profile')</a>
+                                      </div>
+                                      <div class="pull-right">
+                                          <a href="{!! url('/logout') !!}" class="btn btn-default btn-flat"
+                                              onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                              @lang('app.buttons.sign_out')
+                                          </a>
+                                          <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                              {{ csrf_field() }}
+                                          </form>
+                                      </div>
+                                  </li>
+                              </ul>
+                          </li>
+                      </ul>
+                  </div>
+              </nav>
+          </header>
 
-        <!-- Left side column. contains the logo and sidebar -->
-        @include('layouts.sidebar')
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            @yield('content')
-        </div>
+          <!-- Left side column. contains the logo and sidebar -->
+          @include('layouts.sidebar')
+          <!-- Content Wrapper. Contains page content -->
+          <div class="content-wrapper">
+              @yield('content')
+          </div>
 
-        <!-- Main Footer -->
-        <footer class="main-footer" style="max-height: 100px;text-align: center">
-            <strong>© {{ date('Y') }} <a href="{{ route('admin.home') }}">Laravel Starter</a>.</strong> @lang('template.rights_reserved')
-        </footer>
+          <!-- Main Footer -->
+          <footer class="main-footer" style="max-height: 100px;text-align: center">
+              <strong>© {{ date('Y') }} <a href="{{ route('admin.home') }}">Laravel Starter</a>.</strong> @lang('template.rights_reserved')
+          </footer>
 
-    </div>
-@else
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+      </div>
+  @else
+      <nav class="navbar navbar-default navbar-static-top">
+          <div class="container">
+              <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+                  <!-- Collapsed Hamburger -->
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                          data-target="#app-navbar-collapse">
+                      <span class="sr-only">Toggle Navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{!! url('/') !!}">
-                    InfyOm Generator
-                </a>
-            </div>
+                  <!-- Branding Image -->
+                  <a class="navbar-brand" href="{!! url('/') !!}">
+                      InfyOm Generator
+                  </a>
+              </div>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{!! url('/home') !!}">Home</a></li>
-                </ul>
+              <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                  <!-- Left Side Of Navbar -->
+                  <ul class="nav navbar-nav">
+                      <li><a href="{!! url('/home') !!}">Home</a></li>
+                  </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    <li><a href="{!! url('/login') !!}">Login</a></li>
-                    <li><a href="{!! url('/register') !!}">Register</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+                  <!-- Right Side Of Navbar -->
+                  <ul class="nav navbar-nav navbar-right">
+                      <!-- Authentication Links -->
+                      <li><a href="{!! url('/login') !!}">Login</a></li>
+                      <li><a href="{!! url('/register') !!}">Register</a></li>
+                  </ul>
+              </div>
+          </div>
+      </nav>
 
-    <div id="page-content-wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    @yield('content')
-                </div>
-            </div>
-        </div>
-    </div>
-    @endif
+      <div id="page-content-wrapper">
+          <div class="container-fluid">
+              <div class="row">
+                  <div class="col-lg-12">
+                      @yield('content')
+                  </div>
+              </div>
+          </div>
+      </div>
+      @endif
 
-    <!-- jQuery 3.1.1 -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
+      <!-- jQuery 3.1.1 -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
 
-    <!-- AdminLTE App -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/js/app.min.js"></script>
+      <!-- AdminLTE App -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/js/app.min.js"></script>
 
-    @yield('scripts')
-</body>
+      @yield('scripts')
+  </body>
 </html>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,16 @@
 -->
 <html>
 	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116407373-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-116407373-1');
+    </script>
+		
 		<title>lStarter - Laravel + InfyOm + ACL Nativa + Multi Idiomas</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
